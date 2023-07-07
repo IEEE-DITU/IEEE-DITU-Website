7e51b8c3bf497cca51c02387ddeb456348b8bdfb
Shelly243 <108860947+Shelly243@users.noreply.github.com>
2023-07-07 16:47:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import EventsPage from "./EventsPage";
+import TeamsPage from "./TeamsPage";
 
+//LINK
 function App() {
   return (
-    <div className="App">
-      <p>IEEE DITU</p>
-    </div>
+    <Routes>
+      <Route path="/home" element={<HomePage/>}></Route>
+      <Route path="/about" element={<AboutPage/>}></Route>
+      <Route path="/events" element={<EventsPage/>}></Route>
+      <Route path="/teams" element={<TeamsPage/>}></Route>
+    </Routes>
   );
 }
 
